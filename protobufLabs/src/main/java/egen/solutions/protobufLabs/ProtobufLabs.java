221375030668f5c0344fbdac9dafe6b9717fac35
kesavasreeram <c.kesava.talabattula@uptake.com>
2017-04-14 17:03:15 -0500

    feat(protocol-buffer): protocol buffers first tech talk

--- protobufLabs/src/main/java/egen/solutions/protobufLabs/ProtobufLabs.java
+++ protobufLabs/src/main/java/egen/solutions/protobufLabs/ProtobufLabs.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import egen.solutions.protocol.buffers.HackerStudentOuterClass.HackerStudent;
+import egen.solutions.protocol.buffers.LibraryOuterClass.Library;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student.Book;
 import egen.solutions.protocol.buffers.StudentOuterClass.Student.CheckOut;
@@ -21,6 +22,65 @@ public class ProtobufLabs {
 	
 	public static void main(String[] args){
 		System.out.println("Welcome to protoocol buffers");
+		Library myLibrary = null;
+		
+		// lets build a student object
+		Student kesava = Student.newBuilder().setName("Kesava").setAddress("Operating at Egen Solutions").build();
+		System.out.println(kesava.toString());
+		System.out.println(BREAKER);
+		
+		// lets define a books owned by Kesava
+		Book kaneAndAbel = Book.newBuilder().setBookName("Kane And Abel").setAuthor("Jeffrey Archer").build();
+		Book codeComplete = Book.newBuilder().setBookName("Code complete").setAuthor("Steve McConnel").build();
+		Book[] booksOwned = { kaneAndAbel, codeComplete };
+		
+		// Kesava starts to learn
+		Student kesava1 = Student.newBuilder().setName("Kesava").setAddress("Operating at Egen Solutions")
+				.addAllBooksOwned(Arrays.asList(booksOwned)).build();
+		System.out.println(kesava1.toString());
+		System.out.println(BREAKER);
+		
+		// adding checkouts
+		CheckOut book1Checkout = CheckOut.newBuilder().setStudentName("Kesava").setBookName("Kane and Abel")
+				.setDueDate(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(2)).build();
+		CheckOut book2Checkout = CheckOut.newBuilder().setStudentName("Kesava").setBookName("codeComplete")
+				.setDueDate(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(4)).build();
+		CheckOut[] booksCheckedOut = { book1Checkout, book2Checkout };
+
+		// with maps
+		Student kesava2 = Student.newBuilder().setName("Kesava").setAddress("Operating at Egen Solutions")
+				.putBooksDue("Kane And Abel", System.currentTimeMillis())
+				.putBooksDue("Hack the Matrix", System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)).build();
+		System.out.println(kesava2.toString());
+		System.out.println(BREAKER);
+
+		// convert to JSON
+		System.out.println("Lets look at a Json representation.");
+		String secretsOfKesava = getJSonFromProject(codeComplete);
+		System.out.println(secretsOfKesava);
+		System.out.println(BREAKER);
+		
+		// convert to String
+		System.out.println("Lets get it back from the string");
+		Book bookOfSecrets = getProjectFromJSONString(secretsOfKesava);
+		System.out.println(bookOfSecrets.toString());
+		
+		// parsing
+		System.out.println(BREAKER);
+		System.out.println("But protobufs aren't secure");
+		byte[] kesavaByteArr12 = kesava2.toByteArray();
+
+		// now we are comparing two diff type of objects, Student type object and HackerStudent object type
+		// since the first two properties are matching it will still parse them.
+		// this is used by hackers to get the data using hit-and-run technique and get your data unless you
+		// encrypt the byteAppy before sending it over http
+		try {
+			HackerStudent kesava34 = HackerStudent.parseFrom(kesavaByteArr12);
+			System.out.println(kesava34.toString());
+		} catch (InvalidProtocolBufferException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main1(String[] args) {
